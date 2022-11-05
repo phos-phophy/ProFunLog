5a1c5a6e54b49a16bc77ff30dee8336e0eaa3ccf
Artem Kudisov <phos_phophyllite@mail.ru>
2022-11-06 01:32:02 +0300

    Code cleanup

--- prolog_homework/tree.pl
+++ prolog_homework/tree.pl
@@ -1,54 +1,56 @@
-/*Предикаты для работы с деревьями*/
+/* Предикаты для работы с деревьями*/
 
 /* tree_depth(T, N): N - глубина дерева (т.е. количество ребер в самой длинной ветвки дерева)
- * 
- * Допустимые варианты использования:
- * (i, i): проверяет, является ли N глубинной дерева
- *     true:
- *          * tree_depth(nil, -1)
- *          * tree_depth(tree(nil, nil, a), 0)
- *          * tree_depth(tree(tree(nil, nil, b), nil, a), 1)
- *     false:
- *          * tree_depth(tree(tree(nil, nil, b), tree(nil, nil, c), a), 2)
- * (i, o): ищет глубину дерева
- * (o, o): возвращает дерево tree(nil, nil, <любое число>) глубины 0
- *
- * Недопустимые варианты использования:
- * (o, i): возвращает false при N > 1*/
+
+Допустимые варианты использования:
+* (i, i): проверяет, является ли N глубинной дерева
+      true:
+          * tree_depth(nil, -1)
+          * tree_depth(tree(nil, nil, a), 0)
+          * tree_depth(tree(tree(nil, nil, b), nil, a), 1)
+      false:
+          * tree_depth(tree(tree(nil, nil, b), tree(nil, nil, c), a), 2)
+* (i, o): ищет глубину дерева
+* (o, o): возвращает дерево tree(nil, nil, <любое число>) глубины 0
+
+Недопустимые варианты использования:
+* (o, i): возвращает false при N > 1
+*/
 tree_depth(tree(nil, nil, _), 0):- !.
 tree_depth(nil, -1):- !.
 tree_depth(tree(T1, T2, _), N):- tree_depth(T1, N1), tree_depth(T2, N2), N is max(N1, N2) + 1.
 
 
 /* sub_tree(T1, T2): дерево T1 является непустым поддеревом дерева T2
- * 
- * Допустимые вварианты использования:
- * (i, i): проверет, является ли T1 поддеревом дерева T2
- *     true:
- *          * sub_tree(tree(nil, nil, d), tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
- *          * sub_tree(tree(tree(nil, nil, d), nil, b), tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
- *     false:
- *          * sub_tree(tree(nil, nil, a), tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
- * (o, i): выводит все поддеревья
- *     ?- sub_tree(X, tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
- *     > tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a)
- *     > tree(tree(nil, nil, d), nil, b)
- *     > tree(nil, nil, d)
- *     > tree(nil, nil, c)
- *     > false
- * (o, o): генерируются случайные правые деревья (без левых ветвей)
- * 
- * Недопустимые варианты использования:
- * (i, o): выводит все деревья T2, содержащие T1, но вместо правых ветвей в этом поддереве генерируются числа*/
+
+Допустимые вварианты использования:
+* (i, i): проверет, является ли T1 поддеревом дерева T2
+      true:
+          * sub_tree(tree(nil, nil, d), tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
+          * sub_tree(tree(tree(nil, nil, d), nil, b), tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
+      false:
+          * sub_tree(tree(nil, nil, a), tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
+* (o, i): выводит все поддеревья
+      ?- sub_tree(X, tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a))
+      => tree(tree(tree(nil, nil, d), nil, b), tree(nil, nil, c), a)
+      => tree(tree(nil, nil, d), nil, b)
+      => tree(nil, nil, d)
+      => tree(nil, nil, c)
+      => false
+* (o, o): генерируются случайные правые деревья (без левых ветвей)
+
+Недопустимые варианты использования:
+* (i, o): выводит все деревья T2, содержащие T1, но вместо правых ветвей в этом поддереве генерируются числа
+*/
 sub_tree(T1, tree(T21, T22, S2)):- check(T1, tree(T21, T22, S2)); sub_tree(T1, T21); sub_tree(T1, T22).
 check(nil, nil).
 check(tree(T11, T12, S), tree(T21, T22, S)):- check(T11, T21), check(T12, T22).
 
 
-/*flatten_tree(T, L): L - список меток всех узлов дерева ("выровненное" дерево)
- * Допустимые варианты использования:
- *  
- * */
+/* flatten_tree(T, L): L - список меток всех узлов дерева ("выровненное" дерево)
+
+Допустимые варианты использования:
+*/
 flatten_tree(tree(nil, nil, S), [S]):- !.
 flatten_tree(tree(T1, nil, S), [S|L1]):- !, flatten_tree(T1, L1).
 flatten_tree(tree(nil, T2, S), [S|L2]):- !, flatten_tree(T2, L2).
@@ -58,28 +60,29 @@ append([X1|R1], L2, [X1|R3]):- append(R1, L2, R3).
 
 
 /* substitute(T1, V, T, T2): T2 - дерево, полученное путем замены всех вхождений V в дереве T1 на терм T
- * Допустимые варианты исопльзования:
- * (i, i, i, i): проверяет, получено ли дерево T2 из T1 путем замены всех вхождений V на T
- *     true:
- *          * substitute(tree(nil, nil, a), a, b, tree(nil, nil, b))
- *          * substitute(tree(nil, tree(nil, nil, c), a), c, d, tree(nil, tree(nil, nil, d), a))
- *     false:
- *          * substitute(tree(nil, tree(nil, nil, b), b), b, a, tree(nil, tree(nil, nil, a), b))
- * (i, i, i, o): строит дерево T2
- *     ?- substitute(tree(tree(nil, nil, 35), tree(tree(tree(nil, nil, 35), nil, 0), tree(nil, nil, 35), b), a), 35, 70, X)
- *     > tree(tree(nil, nil, 70), tree(tree(tree(nil, nil, 70), nil, 0), tree(nil, nil, 70), b), a)
- * (i, i, o, i): ищет T
- *     ?- substitute(tree(nil, tree(nil, nil, b), b), b, X, tree(nil, tree(nil, nil, a), a))
- *     > X=a
- * (i, o, i, i): ищет V
- *     ?- substitute(tree(nil, tree(nil, nil, b), b), X, a, tree(nil, tree(nil, nil, a), a))
- *     > X=b
- *     ?- substitute(tree(nil, tree(nil, nil, b), b), X, a, tree(nil, tree(nil, nil, a), c))
- *     > false
- * (o, i, i, i): строит T1 (обратаная замена)
- *     ?- substitute(X, b, a, tree(nil, tree(nil, nil, a), c))
- *     > tree(nil, tree(nil, nil, b), c)
- * */
+
+Допустимые варианты использования:
+* (i, i, i, i): проверяет, получено ли дерево T2 из T1 путем замены всех вхождений V на T
+      true:
+          * substitute(tree(nil, nil, a), a, b, tree(nil, nil, b))
+          * substitute(tree(nil, tree(nil, nil, c), a), c, d, tree(nil, tree(nil, nil, d), a))
+      false:
+          * substitute(tree(nil, tree(nil, nil, b), b), b, a, tree(nil, tree(nil, nil, a), b))
+* (i, i, i, o): строит дерево T2
+      ?- substitute(tree(tree(nil, nil, 35), tree(tree(tree(nil, nil, 35), nil, 0), tree(nil, nil, 35), b), a), 35, 70, X)
+      => tree(tree(nil, nil, 70), tree(tree(tree(nil, nil, 70), nil, 0), tree(nil, nil, 70), b), a)
+* (i, i, o, i): ищет T
+      ?- substitute(tree(nil, tree(nil, nil, b), b), b, X, tree(nil, tree(nil, nil, a), a))
+      => X = a
+* (i, o, i, i): ищет V
+      ?- substitute(tree(nil, tree(nil, nil, b), b), X, a, tree(nil, tree(nil, nil, a), a))
+      => X = b
+      ?- substitute(tree(nil, tree(nil, nil, b), b), X, a, tree(nil, tree(nil, nil, a), c))
+      => false
+* (o, i, i, i): строит T1 (обратаная замена)
+      ?- substitute(X, b, a, tree(nil, tree(nil, nil, a), c))
+      => tree(nil, tree(nil, nil, b), c)
+*/
 substitute(nil, _, _, nil):- !.
 substitute(tree(T1, T2, V), V, T, tree(TT1, TT2, T)):- !, substitute(T1, V, T, TT1), substitute(T2, V, T, TT2).
 substitute(tree(T1, T2, K), V, T, tree(TT1, TT2, K)):- dif(K, V), substitute(T1, V, T, TT1), substitute(T2, V, T, TT2).
