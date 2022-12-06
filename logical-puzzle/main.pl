ec96363c7f9705a16af2ddfeb3b081957939ae2f
Artem Kudisov <dark.artbeam@gmail.com>
2022-12-06 04:09:32 +0300

    Implement main menu

---
+++ logical-puzzle/main.pl
@@ -0,0 +1,26 @@
+:- consult('./generate.pl').
+:- use_module(library(pce)). 
+:- initialization start.
+
+start:- 
+    new(@dw, dialog('Logical puzzle')),
+    send(@dw, width(700)),
+    send(@dw, height(700)),
+
+    new(@MainMenu, dialog_group('')),
+    send(@dw, append, @MainMenu),
+    send(@MainMenu, gap, size(250, 30)),
+    send(@MainMenu, append, text('Hello!')),
+
+    new(@ComplexityButtons, dialog_group('Select complexity of puzzle')),
+    send(@MainMenu, append, @ComplexityButtons),
+    send(@ComplexityButtons, gap, size(60, 20)),
+    send(@ComplexityButtons, append, button('Easy', message(@prolog, generate_level_, @dw, @MainMenu, 0))),
+    send(@ComplexityButtons, append, button('Medium', message(@prolog, generate_level_, @dw, @MainMenu, 1)), next_row),
+    send(@ComplexityButtons, append, button('Hard', message(@prolog, generate_level_, @dw, @MainMenu, 2)), next_row),
+ 
+    send(@MainMenu, append, button('Exit', message(@dw, destroy))),
+
+    send(@dw, open).
+
+generate_level_(Frame, HideFrame, Complex):- send(HideFrame, destroy), generate_level(Frame, Complex).
