3c984b1efb93161f38efabca6e586f6a2a32eabc
Artem Kudisov <dark.artbeam@gmail.com>
2022-12-09 03:13:25 +0300

    Add generation of question and answer

--- logic-puzzle/generate.pl
+++ logic-puzzle/generate.pl
@@ -3,6 +3,11 @@ complexity(1, 'Logic puzzle: Medium complexity').
 complexity(2, 'Logic puzzle: High complexity').
 
 
+figure(0, Box, Size):- new(Box, box(Size, Size)).
+figure(1, Circle, Size):- new(Circle, circle(Size)).
+figure(2, Triangle, Size):- new(Triangle, path), send_list(Triangle, append, [point(0, 0), point(Size / 2, -Size), point(Size, 0), point(0, 0)]).
+
+
 % 0 - give answer, 1 - see next question
 :- dynamic mode/1.
 mode(0).
@@ -29,7 +34,7 @@ add_question_sub_menu(Dialog, Picture):-
 
     new(Picture, picture),
     send(Picture, width(500)),
-    send(Picture, height(350)),
+    send(Picture, height(250)),
     send(Question, append, Picture).
 
 
@@ -39,7 +44,7 @@ add_answer_sub_menu(Dialog, Picture, Select):-
 
     new(Picture, picture),
     send(Picture, width(500)),
-    send(Picture, height(50)),
+    send(Picture, height(150)),
     send(Answer, append, Picture),
 
     new(Select, menu('Your answer')),
@@ -55,8 +60,8 @@ add_buttons(Dialog, PictureQ, PictureA, Select, Comp):-
     new(Button, button('Ok', message(@prolog, ok_button_, PictureQ, PictureA, Comp, Select))),
     send(Buttons, append, Button),
 
-    new(Exit, button('Exit', and(message(Dialog, destroy), message(@prolog, start)))),
-    send(Buttons, append, Exit).
+    new(MainMenu, button('Main menu', and(message(Dialog, destroy), message(@prolog, start)))),
+    send(Buttons, append, MainMenu).
 
 
 ok_button_(PictureQ, PictureA, Comp, Select):-
@@ -84,8 +89,52 @@ ok_button(PictureQ, PictureA, 0, Comp, Select):-
     retract(mode(0)).
 
 
-generate_question(PictureQ, _, _):-
-    random(10, 100, R),
-    random(100, 200, X),
-    random(100, 200, Y),
-    send(PictureQ, display, new(_, circle(R)), point(X, Y)).
+generate_question(PictureQ, PictureA, _):-
+    get_types(0, 3, Type1, Type2, Type3, Type4, Type5, Type6, Type7, Type8),
+
+    Size is 100,
+    get(PictureQ, size, SzQ), get(SzQ, height, HQ),
+    HeightQ is HQ / 2 - Size / 2,
+
+    add_simple_figure(PictureQ, Type1, Type2, 100, HeightQ, Size),
+    add_simple_figure(PictureQ, Type2, Type1, 250, HeightQ, Size),
+    add_simple_figure(PictureQ, Type3, Type4, 400, HeightQ, Size),
+
+    get(PictureA, size, SzA), get(SzA, height, HA),
+    HeightA is HA / 2 - Size / 2,
+
+    add_simple_figure(PictureA, Type4, Type3, 100, HeightA, Size),
+    add_simple_figure(PictureA, Type5, Type6, 250, HeightA, Size),
+    add_simple_figure(PictureA, Type7, Type8, 400, HeightA, Size).
+
+
+get_types(From, To, Type1, Type2, Type3, Type4, Type5, Type6, Type7, Type8):-
+    random(From, To, Type1), random(From, To, Type2), random(From, To, Type3), random(From, To, Type4),
+    random(From, To, Type5), random(From, To, Type6), random(From, To, Type7), random(From, To, Type8),
+
+    % guarantee that other answers will not match the correct one
+    diff_pair(Type5, Type6, Type4, Type3),
+    diff_pair(Type7, Type8, Type4, Type3),
+    check_types(Type1, Type2, Type3, Type4, Type5, Type6, Type7, Type8);
+
+    get_types(From, To, Type1, Type2, Type3, Type4, Type5, Type6, Type7, Type8).
+
+
+diff_pair(Type1, Type2, NotType1, NotType2):-
+    not(Type1 is NotType1);
+    not(Type2 is NotType2).
+
+
+check_types(Type1, Type2, Type3, Type4, Type5, Type6, Type7, Type8):-
+    not(Type1 is Type2);
+    diff_pair(Type5, Type6, Type3, Type4),
+    diff_pair(Type7, Type8, Type3, Type4).
+    
+
+add_simple_figure(Picture, TypeBig, TypeSmall, X, Y, Size):-
+    figure(TypeBig, BigFigure, Size),
+    figure(TypeSmall, SmallFigure, Size / 2),
+    
+    send(Picture, display, BigFigure, point(X - Size / 2, Y)),
+    send(Picture, display, SmallFigure, point(X - Size / 4, Y + Size / 4)).
+
