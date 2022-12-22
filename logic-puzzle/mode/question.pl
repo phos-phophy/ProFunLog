e708e4ff814c5996c6348b27fe150847f646e0fc
Artem Kudisov <dark.artbeam@gmail.com>
2022-12-22 16:28:03 +0300

    Add question sign

--- logic-puzzle/mode/question.pl
+++ logic-puzzle/mode/question.pl
@@ -19,9 +19,10 @@ generate_question(PictureQ, PictureA, Comp):-
 
     size(Size), pictureQHeight(HQ), HeightQ is HQ / 2 - Size / 2,
 
-    draw_figure(PictureQ, Desc1, Desc2, 100, HeightQ),  % draw first figure 
-    draw_figure(PictureQ, Desc2, Desc1, 250, HeightQ),  % draw second figure
-    draw_figure(PictureQ, Desc3, Desc4, 400, HeightQ),  % draw third figure
+    draw_figure(PictureQ, Desc1, Desc2, 63, HeightQ),  % draw first figure 
+    draw_figure(PictureQ, Desc2, Desc1, 188, HeightQ),  % draw second figure
+    draw_figure(PictureQ, Desc3, Desc4, 313, HeightQ),  % draw third figure
+    draw_question_sign(PictureQ, 438, HeightQ),
 
     pictureAHeight(HA), HeightA is HA / 2 - Size / 2,
 
@@ -51,3 +52,12 @@ draw_figure(Picture, DescB, DescS, X, Y):-
     send(Picture, display, BigFigure, point(X - Size / 2, Y)),
     send(Picture, display, SmallFigure, point(X - Size / 4, Y + Size / 4)).
 
+
+draw_question_sign(Picture, X, Y):- 
+    new(Sign, path),
+    send_list(Sign, append, [point(0, 20), point(0, 10), point(10, 0), point(40, 0), point(50, 10), point(50, 35), point(30, 50), point(25, 60), point(25, 80)]),
+
+    new(Circ, circle(10)),
+
+    send(Picture, display, Sign, point(X - 25, Y)),
+    send(Picture, display, Circ, point(X - 3, Y + 90)).
