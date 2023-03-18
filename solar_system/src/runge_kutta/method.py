b737fecb9e4b6e16c0e485612ebb27c2f2eff532
Artem Kudisov <dark.artbeam@gmail.com>
2023-03-18 05:39:18 +0300

    Rename variable

--- solar_system/src/runge_kutta/method.py
+++ solar_system/src/runge_kutta/method.py
@@ -6,11 +6,11 @@ import numpy as np
 class RungeKuttaMethod:
 
     @classmethod
-    def solve(cls, x: float, y: np.ndarray, f: Callable[[float, np.ndarray], np.ndarray], h: float) -> np.ndarray:
+    def solve(cls, x: float, u: np.ndarray, f: Callable[[float, np.ndarray], np.ndarray], h: float) -> np.ndarray:
 
-        k1 = f(x, y)
-        k2 = f(x + h / 2, y + h / 2 * k1)
-        k3 = f(x + h / 2, y + h / 2 * k2)
-        k4 = f(x + h, y + h * k3)
+        k1 = f(x, u)
+        k2 = f(x + h / 2, u + h / 2 * k1)
+        k3 = f(x + h / 2, u + h / 2 * k2)
+        k4 = f(x + h, u + h * k3)
 
-        return y + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        return u + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
