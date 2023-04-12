b516e36f5eff2a705830d55c1864f7b9f0dee2b3
Artem Kudisov <dark.artbeam@gmail.com>
2023-04-12 04:06:18 +0300

    Add docstrings for SolarSystem class and add Mars

--- solar_system/src/stars/system.py
+++ solar_system/src/stars/system.py
@@ -8,15 +8,23 @@ from solar_system.src.stars.state import ObjectState
 
 G = 6.67 * 10 ** (-20)  # 'cause we work with kilometers not meters
 
+# For more details about the planets, please refer to https://nssdc.gsfc.nasa.gov/planetary/factsheet/marsfact.html
+
 SUN = CelestialBody('Sun', 'yellow', 1.98 * 10 ** 30, 695.7 * 10 ** 3, 0, 0, 0, 0)  # https://en.wikipedia.org/wiki/Sun
 MERCURY = CelestialBody('Mercury', 'grey', 0.33 * 10 ** 24, 2439, 47796525, -17396512, -18.27, -50.21)
 VENUS = CelestialBody('Venus', 'white', 4.87 * 10 ** 24, 6052, 0, -107712000, -35.19, 0)
-EARTH = CelestialBody('Earth', 'green', 5.972168 * 10 ** 24, 6371, 152097597, 0, 0, -30.2)  # https://en.wikipedia.org/wiki/Earth
-MOON = CelestialBody('Moon', 'white', 0.073 * 10 ** 24, 1738, 152503097, 0, 0, -31.2)
-# JUST = CelestialBody('Just', 'red', 5.972168 * 10 ** 24, 3000, 102097597, 0, -4, -3.2)
+
+EARTH = CelestialBody('Earth', 'green', 5.972168 * 10 ** 24, 6371, 152097597, 0, 0, -29.78)  # https://en.wikipedia.org/wiki/Earth
+MOON = CelestialBody('Moon', 'white', 0.073 * 10 ** 24, 1738, 152503097, 0, 0, -30.78)
+
+MARS = CelestialBody('Mars', 'red', 6.42 * 10 ** 23, 3390, 249232000, 0, 0, -21.97)  # https://en.wikipedia.org/wiki/Mars
+PHOBOS = CelestialBody('Phobos', 'white', 1.072 * 10 ** 16, 11, 249241376, 0, 0, -24.1)
+DEIMOS = CelestialBody('Deimos', 'white', 1.4762 * 10 ** 15, 6, 249208538, 0, 0, -20.6187)
 
 
 class SolarSystem(ObjectState):
+    """ A base class that describes our Solar system with all planets """
+
     def __init__(self, step_size: float):
 
         super(SolarSystem, self).__init__()
@@ -27,36 +35,43 @@ class SolarSystem(ObjectState):
         self._g_weights = 0
 
         self._star = SUN
-        self._planets = [MERCURY, VENUS, EARTH, MOON]
+        self._planets = [MERCURY, VENUS, EARTH, MOON, MARS, PHOBOS, DEIMOS]
         self._comets = []
 
     @property
     def step_size(self):
+        """ Returns a size of the simulating step """
         return self._step_size
 
     @property
     def star(self):
+        """ Returns a system's star """
         return self._star
 
     @property
     def planets(self):
+        """ Returns a list of solar system's planets """
         return self._planets
 
     @property
     def comets(self):
+        """ Returns a list of solar system's comets """
         return self._comets
 
     @property
     def bodies(self):
+        """ Returns a list of all solar system's celestial bodies """
         return [self.star] + self.planets + self.comets
 
     @property
     def states(self):
+        """ Returns a list of the solar system's versions """
         planet_states = {planet.name: planet.states for planet in self._planets}
         return {self._star.name: self._star.states, **planet_states}
 
     @property
     def g_weights(self):
+        """ Returns a list of (G * body.weight) values """
         if self.bodies != self._prev_bodies:
             self._prev_bodies = self.bodies
             self._g_weights = G * np.array([body.weight for body in self.bodies])
@@ -64,25 +79,32 @@ class SolarSystem(ObjectState):
 
     @step_size.setter
     def step_size(self, val: float):
+        """ Sets a size of the simulating step """
         self._step_size = val
 
     def set_state(self, idx: int) -> None:
+        """ Checkouts to the specified version of the solar system """
         for body in self.bodies:
             body.set_state(idx)
 
     def save_state(self) -> None:
+        """ Saves the current state of the solar system """
         for body in self.bodies:
             body.save_state()
 
     def delete_state(self, idx: int) -> None:
+        """ Deletes the specified version of the solar system """
         for body in self.bodies:
             body.delete_state(idx)
 
     def add_comet(self, color: str, weight: int, radius: int, x: float, y: float, speed_x: float, speed_y: float):
+        """ Adds new comet in the solar system """
         comet = CelestialBody('', color, weight, radius, x, y, speed_x, speed_y)
         self._comets.append(comet)
 
     def step(self):
+        """ Calculates new positions and  velocities of system's celestial bodies based on previous values """
+
         u = np.hstack([body.get_position() for body in self.bodies])
 
         u = RungeKuttaMethod.solve_4(0, u, self._get_f(), self._step_size)
@@ -93,7 +115,9 @@ class SolarSystem(ObjectState):
         for body_idx, body in enumerate(bodies, start=1):
             body.set_position(u[4 * body_idx: 4 * body_idx + 4])
 
+    # check theory.ipynb for detailed information
     def _get_f(self) -> Callable[[float, np.ndarray], np.ndarray]:
+        """ Returns a derivative of the function u """
 
         def f(_: float, u: np.ndarray) -> np.ndarray:
             result = u.copy()
