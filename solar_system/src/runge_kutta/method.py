8da809c16d3db0b014fe79933d0ae929127a56b6
Artem Kudisov <dark.artbeam@gmail.com>
2023-04-12 03:29:43 +0300

    Add docstrings for RungeKuttaMethod class

--- solar_system/src/runge_kutta/method.py
+++ solar_system/src/runge_kutta/method.py
@@ -4,9 +4,20 @@ import numpy as np
 
 
 class RungeKuttaMethod:
+    """ A utility class that solves a system of first-degree differential equations u`(x) = f(x, u(x)). It's used by `SolarSystem` to solve
+    the equations of the `CelestialBody`'s motion """
 
     @classmethod
     def solve_2(cls, x: float, u: np.ndarray, f: Callable[[float, np.ndarray], np.ndarray], h: float) -> np.ndarray:
+        """ Solves a system of first-degree differential equations u`(x) = f(x, u(x)) by RK2
+
+        :param x: A given parameter value
+        :param u: A given function
+        :param f: A derivative of the function u
+        :param h: A step size
+        :return: A function value at point x + h
+        """
+
         k1 = f(x, u)
         k2 = f(x + h, u + h * k1)
 
@@ -14,6 +25,15 @@ class RungeKuttaMethod:
 
     @classmethod
     def solve_4(cls, x: float, u: np.ndarray, f: Callable[[float, np.ndarray], np.ndarray], h: float) -> np.ndarray:
+        """ Solves a system of first-degree differential equations u`(x) = f(x, u(x)) by RK4
+
+        :param x: A given parameter value
+        :param u: A given function
+        :param f: A derivative of the function u
+        :param h: A step size
+        :return: A function value at point x + h
+        """
+
         k1 = f(x, u)
         k2 = f(x + h / 2, u + h / 2 * k1)
         k3 = f(x + h / 2, u + h / 2 * k2)
